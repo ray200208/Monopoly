013e560874757ecaa592de4f7457469764fc0039
saketh-3217 <sakshiswags@gmail.com>
2025-11-25 18:46:45 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,67 +1,10 @@
-'''import customtkinter as ctk'''
 import random
 import pygame
 import tkinter as tk
 import time
 import os
+
 pygame.init()
-property_brown=['OLD KENT ROAD','WHITECHAPEL ROAD']
-property_light_blue=['THE ANGEL ISLINGTON','EUSTON ROAD','PENTONVILLE ROAD']
-property_pink=['PALL MALL','WHITEHALL','NORTHUMBERLAND AVENUE']
-property_orange=['BOW STREET','MARLBOROUGH STREET','VINE STREET']
-property_red=['STRAND','FLEET STREET','TRAFALGAR SQUARE']
-property_yellow=['LEICESTER SQUARE','COVENTRY STREET','PICCADILLY']
-property_green=['REGENT STREET','OXFORD STREET','BOND STREET']
-property_dark_blue=['PARK LANE','MAYFAIR']
-property_railway=['KINGS CROSS STATION','MARYLEBONE STATION','FENCHURCH ST. STATION','LIVERPOOL ST. STATION']
-property_utilities=['ELECTRIC COMPANY','WATER WORKS']
-chance_community=['CHANCE','COMMUNITY CHEST']
-free_parking=['FREE PARKING']
-jail=['GO TO JAIL']
-go=['GO']
-visit=['JUST VISITING']
-income_tax=['INCOME TAX']
-super_tax=['SUPER TAX']
-
-
-card_width=80
-card_height=160
-screen=pygame.display.set_mode((600,400))
-font=pygame.font.Font(None,24)
-
-try:
-    image = pygame.image.load("monopoly_logo.jpeg").convert_alpha()
-    print("Image loaded successfully!")
-    
-except Exception as e:
-    print("Image loading failed:", e)
-    image = None
-
-
-import random
-import pygame
-import tkinter as tk
-import time
-import os
-pygame.init()
-property_brown=['OLD KENT ROAD','WHITECHAPEL ROAD']
-property_light_blue=['THE ANGEL ISLINGTON','EUSTON ROAD','PENTONVILLE ROAD']
-property_pink=['PALL MALL','WHITEHALL','NORTHUMBERLAND AVENUE']
-property_orange=['BOW STREET','MARLBOROUGH STREET','VINE STREET']
-property_red=['STRAND','FLEET STREET','TRAFALGAR SQUARE']
-property_yellow=['LEICESTER SQUARE','COVENTRY STREET','PICCADILLY']
-property_green=['REGENT STREET','OXFORD STREET','BOND STREET']
-property_dark_blue=['PARK LANE','MAYFAIR']
-property_railway=['KINGS CROSS STATION','MARYLEBONE STATION','FENCHURCH ST. STATION','LIVERPOOL ST. STATION']
-property_utilities=['ELECTRIC COMPANY','WATER WORKS']
-chance_community=['CHANCE','COMMUNITY CHEST']
-free_parking=['FREE PARKING']
-jail=['GO TO JAIL']
-go=['GO']
-visit=['JUST VISITING']
-income_tax=['INCOME TAX']
-super_tax=['SUPER TAX']
-
 
 card_width=80
 card_height=160
